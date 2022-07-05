3330668a66aa04ff57ed1ec961bb85736d666bb2
Saulux2005 <97383079+Saulux2005@users.noreply.github.com>
2022-07-05 18:21:15 +0300

    SPRING 30

--- src/main/java/lt/bit/products/ui/controller/CustomerSiteController.java
+++ src/main/java/lt/bit/products/ui/controller/CustomerSiteController.java
@@ -1,5 +1,7 @@
 package lt.bit.products.ui.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,10 +12,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 class CustomerSiteController {
 
+    private  final static Logger LOG = LoggerFactory.getLogger(CustomerSiteController.class);
+
     @PostMapping("/cart/add")
     @ResponseBody
-    void addToCart(@RequestParam("productId") String productId) {
-        System.out.println(productId);
+    String addToCart(@RequestParam("productId") String productId) {
+        LOG.info("!!!productId: " + productId);
+        return "Product has been added!";
     }
 
 }
